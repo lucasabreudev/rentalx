8ff2e91b72bc11946c778e62c709f143979f129e
Lucas Abreu <lucasabreu.dev@gmail.com>
2022-09-07 16:31:38 -0300

    refactor: refatoração do código para separar responsabilidades

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,9 +1,5 @@
 import { Category } from '../model/Category';
-
-interface ICreateCreateCategoryDTO {
-  name: string;
-  description: string;
-}
+import { ICreateCategoryDTO } from './ICategoriesRepository';
 
 class CategoriesRepository {
   private categories: Category[];
@@ -12,7 +8,7 @@ class CategoriesRepository {
     this.categories = [];
   }
 
-  create({ name, description }: ICreateCreateCategoryDTO): void {
+  create({ name, description }: ICreateCategoryDTO): void {
     const category = new Category();
 
     Object.assign(category, {
